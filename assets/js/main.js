aae8312de4339165b5b619f4d2a01b43fecd1de0
NAEEMA ZIYAD <75922224+naeema-19@users.noreply.github.com>
2023-02-17 00:05:18 +0530

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,4 @@
-/**
-* Template Name: eNno - v4.10.0
-* Template URL: https://bootstrapmade.com/enno-free-simple-bootstrap-template/
-* Author: BootstrapMade.com
-* License: https://bootstrapmade.com/license/
-*/
+
 (function() {
   "use strict";
 
@@ -154,6 +149,41 @@
     if (window.location.hash) {
       if (select(window.location.hash)) {
         scrollto(window.location.hash)
+      }
+    }
+  });
+   /**
+   * Clients Slider
+   */
+  new Swiper('.clients-slider', {
+    speed: 400,
+    loop: true,
+    autoplay: {
+      delay: 5000,
+      disableOnInteraction: false
+    },
+    slidesPerView: 'auto',
+    pagination: {
+      el: '.swiper-pagination',
+      type: 'bullets',
+      clickable: true
+    },
+    breakpoints: {
+      320: {
+        slidesPerView: 1,
+        spaceBetween: 40
+      },
+      480: {
+        slidesPerView: 2,
+        spaceBetween: 60
+      },
+      640: {
+        slidesPerView: 2,
+        spaceBetween: 80
+      },
+      992: {
+        slidesPerView: 3,
+        spaceBetween: 120
       }
     }
   });
